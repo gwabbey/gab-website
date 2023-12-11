b034999a9395932d36420dea106471a5fc99badf
gab <mail@g3b.dev>
2023-12-11 11:22:03 +0100

    add social buttons

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type {MetaFunction} from "@remix-run/node";
+import {Button, Flex} from "@mantine/core";
+import {IconBrandBandcamp, IconBrandSpotify, IconBrandYoutube} from "@tabler/icons-react";
+import {useHover} from "@mantine/hooks";
 
 export const meta: MetaFunction = () => {
     return [
@@ -8,9 +11,41 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+    const {hovered, ref} = useHover();
     return (
         <div style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.8"}}>
-            <h1>gab</h1>
+            <Flex
+                mih={50}
+                gap="sm"
+                justify="center"
+                align="center"
+                direction="column"
+            >
+                <h1 style={{
+                    fontSize: "3rem",
+                    backgroundImage: 'linear-gradient(to right, rgb(249, 168, 212), rgb(216, 180, 254), rgb(129, 140, 248))',
+                    WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                    fontWeight: 700
+                }}>gab</h1>
+                <div ref={ref}>
+                    <Button size="xl" justify="center" leftSection={<IconBrandSpotify size={32}/>}
+                            variant={hovered ? "outline" : "default"} mt="md" color="green"
+                            radius="xl">
+                        Spotify
+                    </Button>
+                </div>
+                <Button size="xl" justify="center" leftSection={<IconBrandYoutube size={32}/>} variant="default" mt="md"
+                        radius="xl">
+                    YouTube
+                </Button>
+                <Button size="xl" justify="center" leftSection={<IconBrandBandcamp size={32}/>} variant="default"
+                        mt="md"
+                        radius="xl">
+                    Bandcamp
+                </Button>
+            </Flex>
         </div>
     );
 }
+
